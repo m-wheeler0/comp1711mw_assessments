fa6e0afcc62eb3b8b7921c61f036594402b5861b
m-wheeler0 <146672950+m-wheeler0@users.noreply.github.com>
2023-11-17 16:45:36 +0000

    Began Part 2

---
+++ Part2Task1/FitnessDataStruct.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char date[11];
+	char time[6];
+	char steps[6];
+} FITNESS_DATA;
+
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        
+        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
---
+++ Part2Task1/StepCounter.c
@@ -0,0 +1,44 @@
+#include "FitnessDataStruct.h"
+
+int count = 0;
+char strFile[32];
+
+int main() {
+    printf("What option would you like to choose?\n"
+    "A. Specify the filename to be imported\n"
+    "B. Display the total number of records on the file\n"
+    "C. Find the date and time with the fewest steps\n"
+    "D. Find the data and time with the most steps\n"
+    "E. Find the mean step count of all recorded data in the file\n"
+    "F. Find the longest continuous streak of having over 500 steps per record\n"
+    "Q. Quit\n");
+
+    char selection;
+    scanf("%c", &selection);
+
+    if (selection == 'A'){
+        printf("Enter the filename: ");
+        scanf();
+    }
+    else if (selection == 'B'){
+        printf("B\n");
+    }
+    else if (selection == 'C'){
+        printf("C\n");
+    }
+    else if (selection == 'D'){
+        printf("D\n");
+    }
+    else if (selection == 'E'){
+        printf("E\n");
+    }
+    else if (selection == 'F'){
+        printf("F\n");
+    }
+    else if (selection == 'Q'){
+        exit(0);
+    }
+    else{
+        printf("Invalid answer\n");
+    }
+}
