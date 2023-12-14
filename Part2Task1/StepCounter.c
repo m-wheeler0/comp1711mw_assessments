79c8d51a0ff33fc6ffaf41aedaa59620b1cd6186
m-wheeler0 <146672950+m-wheeler0@users.noreply.github.com>
2023-12-14 20:39:09 +0000

    Almost finished, just need to fix F

--- Part2Task1/StepCounter.c
+++ Part2Task1/StepCounter.c
@@ -1,7 +1,7 @@
 #include "FitnessDataStruct.h"
 
 char selection, fileName[32], strFile[32];
-int recordCount, stepCount;
+int recordCount, stepCount, stepTotal;
 FILE *dataFile;
 FITNESS_DATA fitnessData;
 
@@ -19,15 +19,15 @@ int main() {
         scanf(" %c", &selection);
 
         if (selection == 'A'){
-            printf("Enter the filename: ");
+            printf("Input filename: ");
             scanf("%s", fileName);
-            if (fileName == ""){
-                printf("Please input a filename.");
+            if (strcmp(fileName, "") == 0){
+                printf("Input filename.");
                 continue;
             }
             dataFile = fopen(fileName, "r");
             perror("");
-            tokeniseRecord(strFile, ",", fitnessData.date, fitnessData.time, fitnessData.steps);
+            fgets(strFile, 32, dataFile);
             printf("\n");
             
         }
@@ -36,33 +36,83 @@ int main() {
             while (fgets(strFile, 32, dataFile)){
                 recordCount++;
             }
-            printf("The number of records in this data file is: %d\n\n", recordCount);
+            printf("Total records: %d\n\n", recordCount);
+
             recordCount = 0;
         }
         else if (selection == 'C'){
             int lowestStepCount = 20000;
             rewind(dataFile);
             while (fgets(strFile, 32, dataFile)){
+                tokeniseRecord(strFile, ",", fitnessData.date, fitnessData.time, fitnessData.steps);
                 stepCount = atoi(fitnessData.steps);
-                printf("%s", strFile);
-                printf("%s", fitnessData.date);
-                if (stepCount < lowestStepCount && stepCount != 0){
+                if (stepCount < lowestStepCount){
                     lowestStepCount = stepCount;
                 }
 
             }
-            printf("Fewest steps:%d\n", lowestStepCount);
-            perror("");
-            printf("\n\n");
+            printf("Fewest steps: %d\n\n", lowestStepCount);
         }
         else if (selection == 'D'){
-            perror("");
+            int largestStepCount = 0;
+            char largestStepTime[32];
+            rewind(dataFile);
+            while (fgets(strFile, 32, dataFile)){
+                tokeniseRecord(strFile, ",", fitnessData.date, fitnessData.time, fitnessData.steps);
+                stepCount = atoi(fitnessData.steps);
+                if (stepCount > largestStepCount){
+                    largestStepCount = stepCount;
+                    sprintf(largestStepTime ,"Largest steps: %s %s\n", fitnessData.date, fitnessData.time);
+                }
+            }
+            printf("%s\n", largestStepTime);
         }
         else if (selection == 'E'){
-            perror("");
+            rewind(dataFile);
+            stepCount = 0;
+            while (fgets(strFile, 32, dataFile)){
+                tokeniseRecord(strFile, ",", fitnessData.date, fitnessData.time, fitnessData.steps);
+                stepCount = stepCount + atoi(fitnessData.steps);
+                recordCount++;
+            }
+
+            printf("%d\n\n", (stepCount/recordCount));
         }
         else if (selection == 'F'){
-            perror("");
+            char streakStart[32];
+            char longestStreakStart[32];
+            char streakEnd[32];
+            int streakLength = 0;
+            int bestStreak = 0;
+            int currentStreak = 0;
+
+            rewind(dataFile);
+
+            while(fgets(strFile, 32, dataFile)){
+                tokeniseRecord(strFile, ",", fitnessData.date, fitnessData.time, fitnessData.steps);
+                if (atoi(fitnessData.steps) > 500 && streakLength == 0){
+                    sprintf(streakStart, "Longest period start: %s %s\n", fitnessData.date, fitnessData.time);
+                    streakLength ++;
+                }
+                else if(atoi(fitnessData.steps) > 500 && streakLength > 0){
+                    streakLength ++;
+                }
+                else{
+                    if (streakLength > bestStreak){
+                        bestStreak = streakLength;
+                        strcpy(longestStreakStart, streakStart);
+                        sprintf(streakEnd, "Longest period end: %s %s\n", fitnessData.date, fitnessData.time);
+                    } 
+                    else{
+                        continue;
+                    }
+                    streakLength = 0;
+                    
+                }
+            }
+            printf("%s", longestStreakStart);
+            printf("%s", streakEnd);
+            printf("\n");
         }
         else if (selection == 'Q'){
             fclose(dataFile);
